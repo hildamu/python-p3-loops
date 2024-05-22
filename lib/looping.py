5dd81feccab8e8fc22a0f0507011ecf0816fa0d5
hildamu <mukamihilda437@gmail.com>
2024-05-22 12:10:51 +0300

    submit

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+happy_new_year()
+
+def square_integers(nums):
+    if not nums:
+        return None  
+    
+    squared_nums = [num ** 2 for num in nums]  
+    return squared_nums
+
+result = square_integers([1, 2, 3, 4, 5])
+print(result)  
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+fizzbuzz()
